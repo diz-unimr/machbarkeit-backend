78454f58371c7479bb70aaa5613844623dec81b1
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2025-09-24 21:23:07 +0200

    fix: merge session db with requests db

--- migrations/00_init.sql
+++ migrations/00_init.sql
@@ -1,11 +1,20 @@
+create table if not exists users
+(
+    id                  integer primary key autoincrement,
+    name                text not null,
+    email               text not null unique,
+    access_token        text not null
+);
+
 create table if not exists requests
 (
-    id                  uuid not null,
+    id                  uuid primary key,
     date                datetime with timezone not null,
     query               text not null,
     status              text not null,
     result_code         integer,
     result_body         text,
     result_duration     integer,
-    primary key (id)
+    user_id             integer,
+    foreign key (user_id) references users(id)
 );
